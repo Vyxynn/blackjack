8cd2b9390ba03c90da2738bf44779498bafb94e8
Vyxynn <szsilavec+github@gmail.com>
2025-02-18 16:54:19 -0700

    added data

---
+++ script.js
@@ -0,0 +1,15 @@
+let data = [
+    {name: 'ace', display: 'A', amount: 4, score: 1, altScore: 11},
+    {name: 'two', display: '2', amount: 4, score: 2},
+    {name: 'three', display: '3', amount: 4, score: 3},
+    {name: 'four', display: '4', amount: 4, score: 4},
+    {name: 'five', display: '5', amount: 4, score: 5},
+    {name: 'six', display: '6', amount: 4, score: 6},
+    {name: 'seven', display: '7', amount: 4, score: 7},
+    {name: 'eight', display: '8', amount: 4, score: 8},
+    {name: 'nine', display: '9', amount: 4, score: 9},
+    {name: 'ten', display: '10', amount: 4, score: 10},
+    {name: 'queen', display: 'Q', amount: 4, score: 10},
+    {name: 'king', display: 'J', amount: 4, score: 10},
+    {name: 'jack', display: 'J', amount: 4, score: 10}
+];
